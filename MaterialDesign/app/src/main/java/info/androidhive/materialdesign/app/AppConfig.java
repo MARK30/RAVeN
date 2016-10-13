1d931f8f33755c8b267c52537a8d6a7562da2d73
root <root@localhost.localdomain>
2016-10-13 13:22:03 +0530

    Initial Commit

---
+++ MaterialDesign/app/src/main/java/info/androidhive/materialdesign/app/AppConfig.java
@@ -0,0 +1,17 @@
+package info.androidhive.materialdesign.app;
+
+ public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://172.16.48.106/android_login_api/login.php";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://172.16.48.106/android_login_api/register.php";
+	/////////////For mechanic details
+	public static final String DATA_URL = "http://172.16.48.106/volley/getData.php?specialist=";
+	public static final String KEY_NAME = "name";
+	public static final String KEY_PHONE = "phone";
+	public static final String KEY_SPECIALIST = "specialist";
+	 public static final String KEY_LAT = "latitude1";
+	 public static final String KEY_LONG = "longitude1";
+	public static final String JSON_ARRAY = "result";
+}
